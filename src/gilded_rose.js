23cd24f1dd5576b62aac0f8f3ba945cce3b33147
BradGodden <97599978+BradGodden@users.noreply.github.com>
2022-01-12 12:19:47 +0000

    Complete refactor + custom tests

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -11,56 +11,57 @@ export class Shop {
     this.items = items;
   }
   updateQuality() {
-    this.items.forEach(item => {
-      // ^^ loops through the items
-      if (
-        item.name != 'Aged Brie'
-        && item.name != 'Backstage passes to a TAFKAL80ETC concert'
-        && item.quality > 0
-        && item.name != 'Sulfuras, Hand of Ragnaros'
-      ) {
-        // ^^ if the current item's name is not:
-        /*
-        'Aged Brie',
-        'Backstage passes to a TAFKAL80ETC concert',
-        the quality is greater than 0,
-        the current item's name is not 'Sulfuras, Hand of Ragnaros'...
-        */
-        item.quality = item.quality - 1; // decrement the current item's quality by 1
-      } else {
-        if (item.quality < 50) {
-          item.quality = item.quality + 1;
-          if (item.name == 'Backstage passes to a TAFKAL80ETC concert') {
-            if (item.sellIn < 11) {
-              item.quality = item.quality + 1;
-            }
-            if (item.sellIn < 6) {
-              item.quality = item.quality + 1;
-            }
-          }
-        }
+    this.items.map(item => {
+      const minQuality = 0 // the minimum quality
+      const maxQuality = 50 // the maximum quality 
+      const legendary = ['Sulfuras'] // never decreases in quality, never has to be sold.
+      const increasesWithAge = ['Aged Brie'] // increases by 
+      const backstagePasses = ['Backstage passes to a TAFKAL80ETC concert'] // backstage passes, quality increases as expiry comes closer but worthless once concert complete. 
+      const conjuredItems = ['Conjured Mana Cake'] // conjured items test
+      const { name, sellIn, quality } = item
+      if (!legendary.includes(item.name) && !backstagePasses.includes(item.name) && !increasesWithAge.includes(item.name) && !conjuredItems.includes(item.name)) {
+        // standard procedure of decrementing sellin and quality 
+        item.sellIn = item.sellIn - 1; // decrement sellin
+        item.quality = item.quality - 1; // decrement quality
       }
-      if (item.name != 'Sulfuras, Hand of Ragnaros') {
+
+      if (increasesWithAge.includes(item.name)) {
+        // we need to decrease the sellIn but increase the quality 
         item.sellIn = item.sellIn - 1;
+        item.quality = item.quality + 1;
       }
-      if (item.sellIn < 0) {
-        if (item.name != 'Aged Brie') {
-          if (
-            item.name != 'Backstage passes to a TAFKAL80ETC concert'
-            && item.quality > 0
-            && item.name != 'Sulfuras, Hand of Ragnaros'
-          ) {
-              item.quality = item.quality - 1;
-          } else {
-            item.quality = item.quality - item.quality;
-          }
-        } else {
-          if (item.quality < 50) {
-            item.quality = item.quality + 1;
-          }
+
+      if (backstagePasses.includes(item.name)) {
+        // we need to conditionally increment
+        if (item.sellIn < 11) {
+          item.quality = item.quality + 2; // increment item quality.
+        }
+        if (item.sellIn < 6) {
+          item.quality = item.quality + 3; // increment item quality.
         }
       }
+      if (conjuredItems.includes(item.name)) {
+        // we need to degrade this item twice as fast as a regular item. 
+        item.quality = item.quality - 2;
+      }
+
+      // final checks... 
+
+      if(item.quality > maxQuality){
+        item.quality = maxQuality
+      }
+
+      if(item.quality < minQuality){
+        item.quality = minQuality
+      }
+
+      return item;
+
+
     })
+
+
+
 
 
     return this.items;
