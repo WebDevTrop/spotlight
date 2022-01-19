1bc66f66b3db948cd757c5ab95dba855e4bab676
BradGodden <97599978+BradGodden@users.noreply.github.com>
2022-01-19 14:42:56 +0000

    Final piece to make app work

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -19,30 +19,35 @@ export class Shop {
       const backstagePasses = ['Backstage passes to a TAFKAL80ETC concert'] // backstage passes, quality increases as expiry comes closer but worthless once concert complete. 
       const conjuredItems = ['Conjured Mana Cake'] // conjured items test
       const { name, sellIn, quality } = item
-      if (!legendary.includes(item.name) && !backstagePasses.includes(item.name) && !increasesWithAge.includes(item.name) && !conjuredItems.includes(item.name)) {
+      if (!legendary.includes(name) && !backstagePasses.includes(name) && !increasesWithAge.includes(name) && !conjuredItems.includes(name)) {
         // standard procedure of decrementing sellin and quality 
         item.sellIn = item.sellIn - 1; // decrement sellin
         item.quality = item.quality - 1; // decrement quality
       }
 
-      if (increasesWithAge.includes(item.name)) {
+      if (increasesWithAge.includes(name)) {
         // we need to decrease the sellIn but increase the quality 
         item.sellIn = item.sellIn - 1;
         item.quality = item.quality + 1;
       }
 
-      if (backstagePasses.includes(item.name)) {
-        // we need to conditionally increment
+      if (backstagePasses.includes(name)) {
+        // we need to conditionally increment.
+        item.sellIn = item.sellIn - 1;
         if (item.sellIn < 11) {
           item.quality = item.quality + 2; // increment item quality.
         }
         if (item.sellIn < 6) {
           item.quality = item.quality + 3; // increment item quality.
         }
+        if(item.sellIn >= 11){
+          item.quality = item.quality + 1;
+        }
       }
-      if (conjuredItems.includes(item.name)) {
+      if (conjuredItems.includes(name)) {
         // we need to degrade this item twice as fast as a regular item. 
-        item.quality = item.quality - 2;
+        item.quality = item.quality - 2; 
+        item.sellIn = item.sellIn - 1;
       }
 
       // final checks... 
@@ -59,11 +64,6 @@ export class Shop {
 
 
     })
-
-
-
-
-
     return this.items;
   }
 }
