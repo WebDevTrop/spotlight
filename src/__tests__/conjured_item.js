3ad27785e2868fe3a8769aece364e55b0459d465
BradGodden <97599978+BradGodden@users.noreply.github.com>
2022-01-19 14:46:00 +0000

    Simplified conjured test

--- src/__tests__/conjured_item.js
+++ src/__tests__/conjured_item.js
@@ -10,7 +10,7 @@ describe("Conjured Item", function () {
         gildedRose.updateQuality();
         filteredItems = items.map(item => item.name === "Conjured Mana Cake" ? item : null).filter(x => x)
         const quality2 = filteredItems[0].quality
-        const pass = (quality1 - quality2) === 2 ? true : false;
+        const pass = (quality1 - quality2) === 2;
         expect(pass).toEqual(true)
     });
 
